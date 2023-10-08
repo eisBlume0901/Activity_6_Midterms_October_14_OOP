c6a5ee4e5eda15d09b2647d349f5188891b32d6c
Mary Kate Anecito <marykate09@protonmail.com>
2023-10-08 21:30:51 +0800

    Initial Commit

--- src/validator/BirthDateAndPlaceValidator.java
+++ src/validator/BirthDateAndPlaceValidator.java
@@ -1,27 +1,27 @@
 package validator;
 
-import pojo.BirthDateAndPlace;
-import java.util.Scanner;
-import static java.lang.System.*;
-
-public class BirthDateAndPlaceValidator implements ValidationMethod, InputValidator, Searcher
-{
-    private Scanner scanner = new Scanner(in);
-    private BirthDateAndPlace birthDateAndPlace;
-
-    @Override
-    public boolean validate()
-    {
-
-
-        out.println("Birth Place: "); // City
-        String birthPlace = scanner.nextLine();
-
-        if (cityExists(birthPlace))
-        {
-            birthPlace = new BirthDateAndPlace();
-            return true;
-        }
-        return false;
-    }
-}
+//import pojo.BirthDateAndPlace;
+//import java.util.Scanner;
+//import static java.lang.System.*;
+//
+//public class BirthDateAndPlaceValidator implements ValidationMethod, InputValidator, Searcher
+//{
+//    private Scanner scanner = new Scanner(in);
+//    private BirthDateAndPlace birthDateAndPlace;
+//
+//    @Override
+//    public boolean validate()
+//    {
+//
+//
+//        out.println("Birth Place: "); // City
+//        String birthPlace = scanner.nextLine();
+//
+//        if (cityExists(birthPlace))
+//        {
+//            birthPlace = new BirthDateAndPlace();
+//            return true;
+//        }
+//        return false;
+//    }
+//}
